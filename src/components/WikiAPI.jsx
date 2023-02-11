10de66c5ed2a0693654f58b64bd2eaac9109dcb3
Kasib <kasibabdullah8@gmail.com>
2023-02-11 12:07:57 -0500

    made basic components and styling

---
+++ src/components/WikiAPI.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react"
+import './WikiAPI.css'
+
+// goal here is to get input from user, make call to wikipedia to return an article based on input, thinking, input with onChange then onSubmit, pass to the api call and return result in a p tag
+
+function WikiAPI() {
+
+  return (
+    <div className="wiki">
+      <h3>wikiAPI Component</h3>  
+    </div>
+  )
+}
+
+export default WikiAPI
